6ae2c1d914c2b85bedc73852b58e4ac3f1423de7
Federico Bobbio <federicobobbio@gmail.com>
2014-05-16 11:53:35 -0300

    Cambios menores
    Arreglo imports y agrego a la lista de ignore

--- src/main/java/core/MEventController.java
+++ src/main/java/core/MEventController.java
@@ -1,8 +1,5 @@
 package core;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
