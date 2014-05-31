bbffaf09df76b632b9ccbe6f6c48595ea0eedaf7
Federico Bobbio <federicobobbio@gmail.com>
2014-05-31 16:40:14 -0300

    Sigo arreglando

--- src/main/java/controllers/MEventController.java
+++ src/main/java/controllers/MEventController.java
@@ -1,8 +1,10 @@
-package core;
+package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import utils.*;
 
 @Controller
 public class MEventController {
