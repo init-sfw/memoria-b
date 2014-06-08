58fb1617cd9f894375cbf27d8e9e78b42a7e397b
Federico Bobbio <federicobobbio@gmail.com>
2014-06-08 17:08:53 -0300

    Modifico formato de las fechas generales para el formato JSON

--- memoria-backend/grails-app/conf/BootStrap.groovy
+++ memoria-backend/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import grails.converters.JSON
+
 class BootStrap {
 
     def init = { servletContext ->
+	// Parseo para el retorno de fechas por json
+        JSON.registerObjectMarshaller(Date) {
+           return it?.format("dd/MM/yyyy")
+	}
     }
     def destroy = {
     }
