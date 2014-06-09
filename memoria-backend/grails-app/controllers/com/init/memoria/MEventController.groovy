00f93b12549b93c35192c58e368d057e1a491a0d
Federico Bobbio <federicobobbio@gmail.com>
2014-06-09 17:40:32 -0300

    Avances en el uso de parámetros y búsqueda, todavía sin funcionar

--- memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
+++ memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
@@ -11,5 +11,15 @@ class MEventController {
 		html mEventList:events
 		json { render events as JSON }
 		   } 
-	       }
+	}
+
+    def filter() {
+	def events = MEvent.findAll("""from MEvent as m where 
+		m.date between :fromDate and :toDate 
+		and m.rate=:view 
+		and m.category.id IN (:categories) 
+		and m.country.id IN (:countries)""", [params.fromDate, params.toDate, params.view, params.categories, params.countries])
+
+	render events as JSON
+	}
 }
