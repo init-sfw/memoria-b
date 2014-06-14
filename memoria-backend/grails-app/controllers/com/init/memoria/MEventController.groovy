d3e7c18a05756b39c92d5408d6dde0914509f7b4
Federico Bobbio <federicobobbio@gmail.com>
2014-06-14 20:43:08 -0300

    Hago funcionar el WS completo con categorías y países, sólo falta validar cuando no se provee el parámetro country o category en el request, que ahora está fallando

--- memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
+++ memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
@@ -14,11 +14,10 @@ class MEventController {
 	}
 
     def filter() {
-	def events = MEvent.findAll("""from MEvent as m where 
-		m.date between :fromDate and :toDate 
-		and m.rate=:view 
-		and m.category.id IN (:categories) 
-		and m.country.id IN (:countries)""", [params.fromDate, params.toDate, params.view, params.categories, params.countries])
+	def categories = params.list('category').collect{a -> a.toInteger() }
+	def countries = params.list('country').collect{a -> a.toInteger() }
+
+	def events = MEvent.findAll("from MEvent as m where m.date between :fromDate and :toDate and m.rate<=:view and m.category.id IN :categories and m.country.id IN :countries", [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view'), categories: categories, countries: countries])
 
 	render events as JSON
 	}
