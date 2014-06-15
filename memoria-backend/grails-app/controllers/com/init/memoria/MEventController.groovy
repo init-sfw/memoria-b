e19fd290cb80656a6d33a4a09e0149248a6c7d04
Federico Bobbio <federicobobbio@gmail.com>
2014-06-15 11:37:25 -0300

    Arreglo chanchamente los casos en los que no hay parámetro de categoría o de país o ninguno, falta testear full

--- memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
+++ memoria-backend/grails-app/controllers/com/init/memoria/MEventController.groovy
@@ -4,21 +4,51 @@ import grails.converters.JSON
 
 class MEventController {
 
-    def list() {
-	def events = MEvent.list()
+	def list() {
+		def events = MEvent.list()
 
-	withFormat {		
-		html mEventList:events
-		json { render events as JSON }
-		   } 
+		withFormat {		
+			html mEventList:events
+			json { render events as JSON }
+		} 
 	}
 
-    def filter() {
-	def categories = params.list('category').collect{a -> a.toInteger() }
-	def countries = params.list('country').collect{a -> a.toInteger() }
+	def filter() {
+		def events
+		def categories = params.list('category').collect{a -> a.toInteger() }
+		def countries = params.list('country').collect{a -> a.toInteger() }
 
-	def events = MEvent.findAll("from MEvent as m where m.date between :fromDate and :toDate and m.rate<=:view and m.category.id IN :categories and m.country.id IN :countries", [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view'), categories: categories, countries: countries])
+		def query = "from MEvent as m where m.date between :fromDate and :toDate and m.rate <= :view"
+		query = query + prepareArrayQuery(categories, 'category')
+		query = query + prepareArrayQuery(countries, 'country')
+		
+		//FIXME: Fix this messi properly
+		if (categories.size() == 0 && countries.size() == 0) {
+			events = MEvent.findAll(query, [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view')])			
+		}
+		else {
+			if (categories.size() != 0 && countries.size() != 0) {
+				events = MEvent.findAll(query, [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view'), category: categories, country: countries])
+			}
+			else {
+				if (categories.size() == 0) {
+					events = MEvent.findAll(query, [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view'), country: countries])
+				}
+				else {
+					events = MEvent.findAll(query, [fromDate: params.date('fromDate', 'dd/MM/yyyy'), toDate: params.date('toDate', 'dd/MM/yyyy'), view: params.int('view'), category: categories])
+				}
+			}
+		}
 
-	render events as JSON
+		render events as JSON
+	}
+
+	def prepareArrayQuery (array, property) {
+		if (array.size() > 0) {
+			return " and m." + property + ".id IN :" + property
+		}
+		else {
+			return ""
+		}
 	}
 }
